de4266457c115b3e5b18a1b8639825162012c052
Yang Xiaofeng <n.akr.akiiya@gmail.com>
2013-03-25 23:29:37 +0800

    Update packages.

---
+++ elpa/pos-tip-20121128.2315/pos-tip-autoloads.el
@@ -0,0 +1,18 @@
+;;; pos-tip-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("pos-tip-pkg.el" "pos-tip.el") (20816
+;;;;;;  27170 354455))
+
+;;;***
+
+(provide 'pos-tip-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; pos-tip-autoloads.el ends here
